fa3e667d9ac1ea868e556fdb00f8e9c93fd00d96
BrightXiaoHan <beatmight@gmail.com>
2024-07-22 20:13:30 +0800

    remove markdown preview settings

--- lua/options.lua
+++ lua/options.lua
@@ -6,10 +6,6 @@ require "nvchad.options"
 -- o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.scrolloff = 8
 
-vim.g.mkdp_echo_preview_url = 1
-vim.g.mkdp_open_to_the_world = 1
-vim.g.mkdp_port = 33333
-
 -- disable mouse
 vim.opt.mouse = ""
 
