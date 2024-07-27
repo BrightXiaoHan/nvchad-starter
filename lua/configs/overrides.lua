687e56e702730d8d1c047e23f3a914709f1f2363
BrightXiaoHan <beatmight@gmail.com>
2024-07-27 13:41:50 +0800

    remove mapping 'p' in nvim-tree

--- lua/configs/overrides.lua
+++ lua/configs/overrides.lua
@@ -50,7 +50,6 @@ local function nvimtree_attach(bufnr)
   local open_preview = require("cmd").nvim_tree_open_preview
 
   vim.keymap.set("n", "l", open_preview, opts "Open: Preview")
-  vim.keymap.set("n", "p", api.node.open.preview, opts "Open: Preview")
   vim.keymap.set("n", "v", api.node.open.vertical, opts "Open: Vertical Split")
   vim.keymap.set("n", "h", api.node.open.horizontal, opts "Open: Horizontal Split")
 end
