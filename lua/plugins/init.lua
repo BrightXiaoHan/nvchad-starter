05ac853c92b6213b96bdd79501278a0fe1b77eb5
BrightXiaoHan <beatmight@gmail.com>
2024-08-28 22:57:44 +0800

    add cursor like plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -102,6 +102,30 @@ local plugins = { -- Override plugin definition options
     config = true,
     lazy = false,
   },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    -- build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+    -- build = "powershell -ExecutionPolicy Bypass -File Build-LuaTiktoken.ps1",
+    opts = {
+      -- add any opts here
+      provider = "copilot",
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below is optional, make sure to setup it properly if you have lazy=true
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 }
 
 return plugins
