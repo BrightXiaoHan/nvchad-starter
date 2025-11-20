ed36d730ecd00d0bd2531767811d593b086e47e2
BrightXiaoHan <beatmight@gmail.com>
2025-11-20 12:29:24 +0800

    feat(plugins): support local development for trzsz and update dependencies
    Add conditional logic to use local nvim-trzsz repository when available at ~/gitrepo/nvim-trzsz, falling back to the remote version otherwise. This enables easier local development and testing of the trzsz plugin.
    
    Update multiple plugin dependencies to their latest versions including aerial.nvim, base46, conform.nvim, fzf-lua, gitsigns.nvim, lazy.nvim, neogit, nvim-cmp, nvim-lspconfig, nvim-tree.lua, and telescope.nvim.

--- lua/plugins/trzsz.lua
+++ lua/plugins/trzsz.lua
@@ -1,7 +1,22 @@
-local plugin = {
-  "BrightXiaoHan/nvim-trzsz",
-  dependencies = { "nvim-tree/nvim-tree.lua" },
-  keys = {
+local local_trzsz = vim.fn.expand("~/gitrepo/nvim-trzsz")
+local use_local = vim.fn.isdirectory(local_trzsz) == 1
+
+local plugin
+
+if use_local then
+  plugin = {
+    dir = local_trzsz,
+    name = "nvim-trzsz",
+    dependencies = { "nvim-tree/nvim-tree.lua" },
+  }
+else
+  plugin = {
+    "BrightXiaoHan/nvim-trzsz",
+    dependencies = { "nvim-tree/nvim-tree.lua" },
+  }
+end
+
+plugin.keys = {
     {
       "<M-r>",
       function()
@@ -29,10 +44,9 @@ local plugin = {
       noremap = true,
       silent = false,
     },
-  },
-  cond = function()
-    return not vim.g.vscode
-  end,
-}
+  }
+plugin.cond = function()
+  return not vim.g.vscode
+end
 
 return plugin
