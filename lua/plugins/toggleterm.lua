f8b8aa2d87486a4f3d55a8eb4f9757e102852294
BrightXiaoHan <beatmight@gmail.com>
2025-11-24 23:05:07 +0800

    Update Codex toggle keybinding

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,37 +1,76 @@
 -- if win32, use pwsh
 -- otherwise, use fish
 if vim.fn.has "win32" == 1 then
-  SHELL = "pwsh"
+    SHELL = "pwsh"
 else
-  SHELL = "fish"
+    SHELL = "fish"
 end
 
 local opts = {
-  shell = SHELL,
+    shell = SHELL,
 }
 
+local function make_codex_toggle()
+    local Terminal = require("toggleterm.terminal").Terminal
+    local codex_term = Terminal:new {
+        cmd = "codex",
+        direction = "vertical",
+        count = 99,
+        size = function()
+            return math.floor(vim.o.columns / 3)
+        end,
+        hidden = true,
+    }
+
+    return function()
+        codex_term:toggle()
+        -- keep the Codex terminal docked on the right
+        if codex_term:is_open() then
+            vim.cmd "wincmd L"
+        end
+    end
+end
+
+local function make_tab_toggle()
+    -- force terminal 1 to always open in a new tab, regardless of prior state
+    return function()
+        require("toggleterm").toggle(1, nil, nil, "tab")
+    end
+end
+
 local plugin = {
-  "akinsho/toggleterm.nvim",
-  config = true,
-  lazy = false,
-  opts = opts,
-  init = function()
-    vim.keymap.set("n", "<C-\\>", "<cmd>ToggleTerm direction=horizontal<CR>", {
-      desc = "Toggle horizontal term",
-    })
-    vim.keymap.set("t", "<C-\\>", "<cmd>ToggleTerm direction=horizontal<CR>", {
-      desc = "Toggle horizontal term",
-    })
-    vim.keymap.set("n", "<C-`>", "<cmd>ToggleTerm direction=horizontal<CR>", {
-      desc = "Toggle horizontal term",
-    })
-    vim.keymap.set("t", "<C-`>", "<cmd>ToggleTerm direction=horizontal<CR>", {
-      desc = "Toggle horizontal term",
-    })
-  end,
-  cond = function()
-    return not vim.g.vscode
-  end,
+    "akinsho/toggleterm.nvim",
+    config = function(_, _opts)
+        require("toggleterm").setup(_opts)
+
+        local toggle_codex = make_codex_toggle()
+        vim.keymap.set({ "n", "t" }, "<leader>tc", toggle_codex, {
+            desc = "Toggle Codex terminal",
+        })
+        vim.keymap.set({ "n", "t" }, "<A-l>", toggle_codex, {
+            desc = "Toggle Codex terminal",
+        })
+    end,
+    lazy = false,
+    opts = opts,
+    init = function()
+        local toggle_tab = make_tab_toggle()
+        vim.keymap.set("n", "<C-\\>", toggle_tab, {
+            desc = "Toggle tab term",
+        })
+        vim.keymap.set("t", "<C-\\>", toggle_tab, {
+            desc = "Toggle tab term",
+        })
+        vim.keymap.set("n", "<C-`>", toggle_tab, {
+            desc = "Toggle tab term",
+        })
+        vim.keymap.set("t", "<C-`>", toggle_tab, {
+            desc = "Toggle tab term",
+        })
+    end,
+    cond = function()
+        return not vim.g.vscode
+    end,
 }
 
 return plugin
