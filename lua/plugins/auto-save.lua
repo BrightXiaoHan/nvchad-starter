18383150a3705df819de2982313867765b13ec99
BrightXiaoHan <beatmight@gmail.com>
2025-12-20 14:44:42 +0800

    Limit auto-save to insert-mode changes and add debounce to reduce delete lag

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,8 +1,11 @@
 local plugin = {
   "okuuva/auto-save.nvim",
   cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
-  opts = {},
+  event = { "InsertLeave", "TextChangedI" }, -- optional for lazy loading on trigger events
+  opts = {
+    debounce_delay = 1000,
+    trigger_events = { "InsertLeave", "TextChangedI" },
+  },
   keys = {
     { "<leader>n", "<cmd>ASToggle<CR>", desc = "Toggle auto-save" },
   },
