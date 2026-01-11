64595f77455ef29cc43c9f773d027b80da707179
BrightXiaoHan <beatmight@gmail.com>
2026-01-11 12:16:01 +0800

    feat(trzsz): refactor key mappings for NvimTree integration

--- lua/plugins/trzsz.lua
+++ lua/plugins/trzsz.lua
@@ -16,35 +16,42 @@ else
   }
 end
 
-plugin.keys = {
-    {
-      "<M-r>",
-      function()
+plugin.ft = "NvimTree"
+
+plugin.config = function()
+  local group = vim.api.nvim_create_augroup("TrzszNvimTreeKeys", { clear = true })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    pattern = "NvimTree",
+    callback = function(args)
+      vim.keymap.set("n", "<M-r>", function()
         require("nvim-trzsz").nvim_tree_trz()
-      end,
-      desc = "Trz: upload file",
-      noremap = true,
-      silent = false,
-    },
-    {
-      "<M-s>",
-      function()
+      end, {
+        desc = "Trz: upload file",
+        noremap = true,
+        silent = false,
+        buffer = args.buf,
+      })
+      vim.keymap.set("n", "<M-s>", function()
         require("nvim-trzsz").nvim_tree_tsz()
-      end,
-      desc = "Tsz: download file",
-      noremap = true,
-      silent = false,
-    },
-    {
-      "<M-o>",
-      function()
+      end, {
+        desc = "Tsz: download file",
+        noremap = true,
+        silent = false,
+        buffer = args.buf,
+      })
+      vim.keymap.set("n", "<M-o>", function()
         require("nvim-trzsz").nvim_tree_open()
-      end,
-      desc = "Tsz: open file",
-      noremap = true,
-      silent = false,
-    },
-  }
+      end, {
+        desc = "Tsz: open file",
+        noremap = true,
+        silent = false,
+        buffer = args.buf,
+      })
+    end,
+  })
+end
 plugin.cond = function()
   return not vim.g.vscode
 end
